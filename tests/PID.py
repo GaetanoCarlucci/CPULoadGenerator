b4c4b3f21ddc48ef6ab5b26f2e11559ddaf0eb76
Manuel Osvaldo Olguín <molguin@kth.se>
2018-06-27 16:08:18 +0200

    fix tests

--- tests/PID.py
+++ tests/PID.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 
 import sys
 import os
+
+import psutil
+
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/..')
 
 from utils.Monitor import MonitorThread
@@ -16,7 +19,7 @@ from utils.Controller import ControllerThread
 from utils.ClosedLoopActuator import ClosedLoopActuator
 
 if __name__ == "__main__":
-   
+
     ######################################################
     #             PID TEST                               # 
     ######################################################
@@ -24,29 +27,34 @@ if __name__ == "__main__":
     # this test aims at characterizing the CPU
     testing = 1
     dynamics_plot_online = 0
+    p = psutil.Process(os.getpid())
+    available_cores = p.cpu_affinity()
+    core = available_cores[0]
+
     if testing == 1:
         cpuSequence = [0.8, 0.1, 0.30, 0.70, 0.40, 0.10, 0.20, 0.60, 0.20, 0.70]
         #cpuSequence = [0.8, 0.1]
         stepPeriod = 4
 
         control = ControllerThread(0.1)
-        monitor = MonitorThread(0, 0.1)
-        actuator = ClosedLoopActuator(control, monitor, len(cpuSequence) * stepPeriod, 0, 1, dynamics_plot_online)
+        monitor = MonitorThread(core, 0.1)
+        actuator = ClosedLoopActuator(control, monitor, len(cpuSequence) *
+                                      stepPeriod, core, 1, dynamics_plot_online)
 
         monitor.start()
         control.start()
         actuator.run_sequence(cpuSequence)
-        
+
         actuator.close()
-        monitor.running = 0
-        control.running = 0
-        dynamics =  monitor.get_dynamics()
-       
+        monitor.stop()
+        control.stop()
+        dynamics = monitor.get_dynamics()
+
         monitor.join()
         control.join()
-        
+
         stepPeriod = 4
-                
+
         with open('pid_data', 'w') as outfile:
             json.dump(dynamics, outfile)
     else:
@@ -62,7 +70,7 @@ if __name__ == "__main__":
     ax1.set_ylim([0, 100])
     for tl in ax1.get_yticklabels():
         tl.set_color('b')
-    
+
     ax2 = plt.twinx()
     ax2.set_ylabel('Input Load', color='r')
     cpuInput = [ 100*(x) for x in dynamics['cpuTarget']]
