4d1f3a3d90e686ba9458ae6b0eeb0ddaa6242ac6
Marcin <marcin.tyman@gmail.com>
2021-03-06 18:35:12 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='cpu-load-generator',
-      version='1.0.1',
+      version='1.0.2',
       description='CPU load generator',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/sirtyman/CPULoadGenerator',
       author='Marcin Tyman',
       author_email='marcin.tyman@gmail.com',
